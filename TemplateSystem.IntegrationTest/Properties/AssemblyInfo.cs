a074350f7f430a40e978639df86e063cd92ee962
israel <ragi.developer@hotmail.com>
2021-04-27 09:27:01 -0500

    First commit

---
+++ TemplateSystem.IntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TemplateSystem.IntegrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TemplateSystem.IntegrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4294321e-53dc-40cc-a810-b70d6901c25f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TemplateSystem.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,93 @@
+﻿using System.Web.Optimization;
+
+
+namespace TemplateSystem.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            RegisterLogin(bundles);
+            RegisterLayout(bundles);
+
+            BundleTable.EnableOptimizations = true;
+
+        }
+
+
+        private static void RegisterLogin(BundleCollection bundles)
+        {
+            //Styles
+            bundles.Add(new StyleBundle("~/assets/plugins/jquery-ui").Include(
+                "~/assets/plugins/jquery-ui/jquery-ui.min.css"));
+
+
+            bundles.Add(new StyleBundle("~/assets/plugins/bootstrap/4.1.0/css").Include(
+               "~/assets/plugins/bootstrap/4.1.0/css/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/plugins/font-awesome/5.0/css").Include(
+               "~/assets/plugins/font-awesome/5.0/css/fontawesome-all.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/plugins/animate").Include(
+               "~/assets/plugins/animate/animate.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/css/material").Include(
+               "~/assets/css/material/style.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/css/material").Include(
+               "~/assets/css/material/style-responsive.min.css"));
+
+            bundles.Add(new StyleBundle("~/assets/css/material/theme").Include(
+               "~/assets/css/material/theme/default.css"));
+
+
+            bundles.Add(new StyleBundle("~/assets/plugins/gritter/css").Include(
+               "~/assets/plugins/gritter/css/jquery.gritter.css"
+           ));
+
+
+            //Scripts          
+            
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/jquery").Include(
+              "~/assets/plugins/jquery/jquery-3.2.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/jquery-ui").Include(
+              "~/assets/plugins/jquery-ui/jquery-ui.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/bootstrap/4.1.0/js").Include(
+              "~/assets/plugins/bootstrap/4.1.0/js/bootstrap.bundle.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/slimscroll").Include(
+              "~/assets/plugins/slimscroll/jquery.slimscroll.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/js-cookie").Include(
+              "~/assets/plugins/js-cookie/js.cookie.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/js/theme").Include(
+              "~/assets/js/theme/material.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/js").Include(
+              "~/assets/js/apps.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/gritter/js").Include(
+              "~/assets/plugins/gritter/js/jquery.gritter.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/plugins/pace").Include(
+              "~/assets/plugins/pace/pace.min.js"));
+
+
+        }
+
+
+        public static void RegisterLayout(BundleCollection bundles)
+        {
+            //Esta es una forma de agregar los estilos minificados
+        }
+
+
+
+
+    }
+}
