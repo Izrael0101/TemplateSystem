3a4f6fa38cc6a79ee66f18699c25adfa09a08d3a
Israel Ramirez <26662639+Izrael0101@users.noreply.github.com>
2021-04-27 09:56:41 -0500

    Commit force

---
+++ TemplateSystem.Repository/StarDescs/StarDescRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using TemplateSystem.Entity.Models;
+using TemplateSystem.Data.Models;
+
+namespace TemplateSystem.Repository.StarDescs
+{
+    public class StarDescRepository : IStarDescRepository, IDisposable
+    {
+
+        private StarSystemContext _context;
+
+        /// <summary>
+        ///     Create a new instance of <see cref="StarDescRepository" />.
+        /// </summary>
+        /// <param name="transaction">Active transaction</param>
+        /// <exception cref="ArgumentNullException">transaction</exception>
+        public StarDescRepository()
+        {
+            _context = new StarSystemContext();
+
+        }
+
+        
+        public async Task<List<StarDesc>> GetStarDescsAsync()
+        {
+            return await _context.StarDescs.ToListAsync();
+        }
+
+        public async Task<StarDesc> GetStarDescByIdAsync(int? id)
+        {
+            return await _context.StarDescs.FindAsync(id);
+        }
+
+        public  async Task CreateStarAsync(StarDesc stardesc)
+        {
+             _context.StarDescs.Add(stardesc);
+            await _context.SaveChangesAsync(); 
+        }
+
+        public async Task EditStarDescAsync(StarDesc stardesc)
+        {
+            _context.Entry(stardesc).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStarAsync(int? id)
+        {
+            StarDesc stardesc = await _context.StarDescs.FindAsync(id);
+            _context.StarDescs.Remove(stardesc);
+            await _context.SaveChangesAsync();
+        }
+           
+        
+       
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        #endregion
+    }
+}
---
+++ TemplateSystem.Web/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TemplateSystem.Entity.Models;
+using NLog;
+
+namespace TemplateSystem.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Login()
+        {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+
+            LoginModel Mod = new LoginModel();
+            return View(Mod);
+        }
+
+       // [HttpPost]
+        public JsonResult LogInAccess()
+        {                       
+            try
+            {               
+                //if(string example)
+
+                var ret = Json(new
+                {
+                    Result = "OK",
+                    url = "Home" + '/' + "Index"
+
+                });
+
+                return ret;
+                //}
+                //else
+                //{
+                //    return Json(new { Result = "ERROR", Message = LoginResponse.Message.Message, ChagePass = LoginResponse.Message.MustChangePassword });
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+
+
+        public JsonResult DeleteSession()
+        {
+            try
+            {
+               
+
+                var ret = Json(new
+                {
+                   
+
+                });
+
+                return ret;
+               
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+
+
+
+        public ActionResult LogOut()
+        {
+            try
+            {
+                Session.Clear();
+                Session.Abandon();
+                Session.RemoveAll();
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                //Se elimina el throw ya que no deberia parar la aplicacion en caso de error
+                Console.WriteLine(ex.Message);
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+
+    }
+}
